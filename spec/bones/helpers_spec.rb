834ac7c1e7be7893d7cb1d00ef884eb410e91b88
Tim Pease <tim.pease@gmail.com>
2010-11-23 13:04:15 -0700

    adding some tests for the "helpers" module

---
+++ spec/bones/helpers_spec.rb
@@ -0,0 +1,17 @@
+
+require 'spec_helper'
+
+module BonesHelpers
+  extend Bones::Helpers
+end
+
+describe Bones::Helpers do
+
+  before :each do
+    Bones::Helpers::HAVE.clear
+  end
+
+  it 'needs some specs'
+
+end
+
