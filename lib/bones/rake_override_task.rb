9cfe5d97f9e64754ee6aa4d69340696c9f4c4a45
Tim Pease <tim.pease@gmail.com>
2012-03-05 10:56:23 -0700

    Adding a "remove_task" method
    The remove_task method can be used to undefine a rake task that you do not
    want to use. This is especially useful when developing gems you do not want
    to release - you can remove the "gem:release" task so that it is not
    inadvertently invoked.

--- lib/bones/rake_override_task.rb
+++ lib/bones/rake_override_task.rb
@@ -6,20 +6,29 @@
 #
 
 Rake::TaskManager.class_eval do
-  def alias_task(fq_name)
+  def alias_task( fq_name )
     new_name = "#{fq_name}:original"
     @tasks[new_name] = @tasks.delete(fq_name)
   end
+
+  def remove_task( fq_name )
+    @tasks.delete(fq_name.to_s)
+  end
 end
 
-def alias_task(fq_name)
+def alias_task( fq_name )
   Rake.application.alias_task(fq_name)
 end
 
-def override_task(*args, &block)
+def override_task( *args, &block )
   name, params, deps = Rake.application.resolve_args(args.dup)
   fq_name = Rake.application.instance_variable_get(:@scope).dup.push(name).join(':')
   alias_task(fq_name)
   Rake::Task.define_task(*args, &block)
 end
+
+def remove_task( *args )
+  args.flatten.each { |fq_name| Rake.application.remove_task(fq_name) }
+end
+alias :remove_tasks :remove_task
 
