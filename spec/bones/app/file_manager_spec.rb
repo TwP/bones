16cff7ab1e4cb78c51dfa7d31d52ca895c044f6e
Tim Pease <tim.pease@gmail.com>
2022-05-29 13:55:14 -0600

    cleaning up expectations

--- spec/bones/app/file_manager_spec.rb
+++ spec/bones/app/file_manager_spec.rb
@@ -14,33 +14,33 @@ describe Bones::App::FileManager do
   end
 
   it "should have a configurable source" do
-    @fm.source.should be_nil
+    expect(@fm.source).to be_nil
 
     @fm.source = '/home/user/.mrbones/default'
-    @fm.source.should be == '/home/user/.mrbones/default'
+    expect(@fm.source).to eq('/home/user/.mrbones/default')
   end
 
   it "should have a configurable destination" do
-    @fm.destination.should be_nil
+    expect(@fm.destination).to be_nil
 
     @fm.destination = 'my_new_app'
-    @fm.destination.should be == 'my_new_app'
+    expect(@fm.destination).to eq('my_new_app')
   end
 
   it "should set the archive directory when the destination is set" do
-    @fm.archive.should be_nil
+    expect(@fm.archive).to be_nil
 
     @fm.destination = 'my_new_app'
-    @fm.archive.should be == 'my_new_app.archive'
+    expect(@fm.archive).to eq('my_new_app.archive')
   end
 
   it "should return a list of files to copy" do
     @fm.source = Bones.path %w[spec data default]
 
     ary = @fm._files_to_copy
-    ary.length.should be == 8
+    expect(ary.length).to eq(8)
 
-    ary.should be == %w[
+    expect(ary).to eq(%w[
       .bnsignore
       .rvmrc.bns
       History
@@ -49,18 +49,18 @@ describe Bones::App::FileManager do
       Rakefile.bns
       bin/NAME.bns
       lib/NAME.rb.bns
-    ]
+    ])
   end
 
   it "should archive the destination directory if it exists" do
     @fm.destination = Bones.path(%w[spec data bar])
-    test(?e, @fm.destination).should be == false
-    test(?e, @fm.archive).should be == false
+    expect(test(?e, @fm.destination)).to be false
+    expect(test(?e, @fm.archive)).to     be false
 
     FileUtils.mkdir @fm.destination
     @fm.archive_destination
-    test(?e, @fm.destination).should be == false
-    test(?e, @fm.archive).should be == true
+    expect(test(?e, @fm.destination)).to be false
+    expect(test(?e, @fm.archive)).to     be true
   end
 
   it "should rename files and folders containing 'NAME'" do
@@ -71,8 +71,8 @@ describe Bones::App::FileManager do
     @fm._rename(File.join(@fm.destination, 'NAME'), 'tirion')
 
     dir = File.join(@fm.destination, 'tirion')
-    test(?d, dir).should be == true
-    test(?f, File.join(dir, 'tirion.rb.bns')).should be == true
+    expect(test(?d, dir)).to be true
+    expect(test(?f, File.join(dir, 'tirion.rb.bns'))).to be true
   end
 
   it "should raise an error when renaming an existing file or folder" do
@@ -80,8 +80,7 @@ describe Bones::App::FileManager do
     @fm.destination = Bones.path(%w[spec data bar])
     @fm.copy
 
-    lambda {@fm._rename(File.join(@fm.destination, 'NAME'), 'lib')}.
-      should raise_error(RuntimeError)
+    expect {@fm._rename(File.join(@fm.destination, 'NAME'), 'lib')}.to raise_error(RuntimeError)
   end
 
   it "should perform ERb templating on '.bns' files" do
@@ -90,18 +89,18 @@ describe Bones::App::FileManager do
     @fm.template('foo_bar')
 
     dir = @fm.destination
-    test(?e, File.join(dir, 'Rakefile.bns')).should be == false
-    test(?e, File.join(dir, 'README.md.bns')).should be == false
-    test(?e, File.join(dir, %w[foo_bar foo_bar.rb.bns])).should be == false
-    test(?e, File.join(dir, '.rvmrc.bns')).should be == false
+    expect(test(?e, File.join(dir, 'Rakefile.bns'))).to be false
+    expect(test(?e, File.join(dir, 'README.md.bns'))).to be false
+    expect(test(?e, File.join(dir, %w[foo_bar foo_bar.rb.bns]))).to be false
+    expect(test(?e, File.join(dir, '.rvmrc.bns'))).to be false
 
-    test(?e, File.join(dir, 'Rakefile')).should be == true
-    test(?e, File.join(dir, 'README.md')).should be == true
-    test(?e, File.join(dir, %w[foo_bar foo_bar.rb])).should be == true
-    test(?e, File.join(dir, '.rvmrc')).should be == true
+    expect(test(?e, File.join(dir, 'Rakefile'))).to be true
+    expect(test(?e, File.join(dir, 'README.md'))).to be true
+    expect(test(?e, File.join(dir, %w[foo_bar foo_bar.rb]))).to be true
+    expect(test(?e, File.join(dir, '.rvmrc'))).to be true
 
     txt = File.read(File.join(@fm.destination, %w[foo_bar foo_bar.rb]))
-    txt.should be == <<-TXT
+    expect(txt).to eq <<-TXT
 module FooBar
   def self.foo_bar
     p 'just a test'
@@ -116,8 +115,8 @@ end
     @fm.template('foo_bar')
 
     dir = @fm.destination
-    test(?e, File.join(dir, 'bin/foo_bar')).should be == true
-    test(?x, File.join(dir, 'bin/foo_bar')).should be == true
+    expect(test(?e, File.join(dir, 'bin/foo_bar'))).to be true
+    expect(test(?x, File.join(dir, 'bin/foo_bar'))).to be true
   end
 
   # ------------------------------------------------------------------------
@@ -125,34 +124,33 @@ end
 
     it "should recognize a git repository" do
       @fm.source = 'git://github.com/TwP/bones.git'
-      @fm.repository.should be == :git
+      expect(@fm.repository).to eq(:git)
 
       @fm.source = 'git://github.com/TwP/bones.git/'
-      @fm.repository.should be == :git
+      expect(@fm.repository).to eq(:git)
     end
 
     it "should recognize an svn repository" do
       @fm.source = 'file:///home/user/svn/ruby/trunk/apc'
-      @fm.repository.should be == :svn
+      expect(@fm.repository).to eq(:svn)
 
       @fm.source = 'http://svn.ruby-lang.org/repos/ruby/branches/ruby_1_8'
-      @fm.repository.should be == :svn
+      expect(@fm.repository).to eq(:svn)
 
       @fm.source = 'https://svn.ruby-lang.org/repos/ruby/branches/ruby_1_8'
-      @fm.repository.should be == :svn
+      expect(@fm.repository).to eq(:svn)
 
       @fm.source = 'svn://10.10.10.10/project/trunk'
-      @fm.repository.should be == :svn
+      expect(@fm.repository).to eq(:svn)
 
       @fm.source = 'svn+ssh://10.10.10.10/project/trunk'
-      @fm.repository.should be == :svn
+      expect(@fm.repository).to eq(:svn)
     end
 
     it "should return nil if the source is not a repository" do
       @fm.source = '/some/directory/on/your/hard/drive'
-      @fm.repository.should be_nil
+      expect(@fm.repository).to be_nil
     end
   end
-
 end
 
